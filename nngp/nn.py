ec105f0c76aed2434629ae4d4a5e7f5b5adc618f
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-03-06 21:22:06 +0000

    Add a simple MLP in haiku

--- nngp/nn.py
+++ nngp/nn.py
@@ -1,6 +1,7 @@
 from typing import Callable, Dict, List
 import jax
 import jax.numpy as jnp
+import haiku as hk
 
 
 def get_mlp(hidden_dim: List[int], activation: str = 'tanh'
@@ -9,6 +10,7 @@ def get_mlp(hidden_dim: List[int], activation: str = 'tanh'
     if activation not in ['relu', 'tanh']:
         raise NotImplementedError("Use either 'relu' or 'tanh' for activation")
     activation_fn = jnp.tanh if activation == 'tanh' else jax.nn.relu
+
     def mlp(X: jnp.ndarray, params: Dict[str, jnp.ndarray]) -> jnp.ndarray:
         """MLP for a single MCMC sample of weights and biases, handling arbitrary number of layers."""
         h = X
@@ -18,3 +20,22 @@ def get_mlp(hidden_dim: List[int], activation: str = 'tanh'
         z = jnp.matmul(h, params[f"w{len(hidden_dim)}"]) + params[f"b{len(hidden_dim)}"]
         return z
     return mlp
+
+
+class MLP(hk.Module):
+    def __init__(self, hidden_dim, latent_dim, activation='tanh', name=None):
+        super().__init__(name=name)
+        if activation not in ['relu', 'tanh']:
+            raise NotImplementedError("Use either 'relu' or 'tanh' for activation")
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+        self.activation_fn = jnp.tanh if activation == 'tanh' else jax.nn.relu
+
+    def __call__(self, x):
+        # Process through each hidden layer
+        for size in self.hidden_dim:
+            x = hk.Linear(output_size=size)(x)
+            x = self.activation(x)
+        # The latent (output) layer
+        x = hk.Linear(output_size=self.latent_dim)(x)
+        return x
