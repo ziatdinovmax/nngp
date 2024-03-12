235264195d486984775c600154b521008014731b
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-03-12 22:21:09 +0000

    Update synthetic function generators

--- nngp/utils.py
+++ nngp/utils.py
@@ -1,41 +1,116 @@
 import numpy as np
 
 
-def piecewise1(x: np.ndarray, params) -> np.ndarray:
-    return np.piecewise(
-        x,
-        [x < params["t"], x >= params["t"]],
-        [lambda x: x**params["beta1"], lambda x: 0.5*x**params["beta2"]])
+def piecewise1(num_points: int = 25, random_seed: int = None) -> np.ndarray:
+
+    def f(x):
+        return np.piecewise(
+            x, [x < 1.7, x >= 1.7],
+            [lambda x: x**4.5, lambda x: 0.5*x**2.5])
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(0, 3, num_points)
+    else:
+        x = np.linspace(0, 3, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
 
 
-def piecewise2(x: np.ndarray):
-    return np.piecewise(
-        x, [x < 5, x >= 5],
-        [lambda x: np.sin(x), lambda x: np.sin(x) + 3])
+def piecewise2(num_points: int = 25, random_seed: int = None):
+
+    def f(x):
+        return np.piecewise(
+            x, [x < 5, x >= 5],
+            [lambda x: np.sin(x), lambda x: np.sin(x) + 3])
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(0, 10, num_points)
+    else:
+        x = np.linspace(0, 10, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
 
 
-def piecewise3(x: np.ndarray) -> np.ndarray:
-    return np.piecewise(
-        x,
-        [x < 1, (x >= 1) & (x < 2), x >= 2],
-        [
-            lambda x: 1 * x**2.5,
-            lambda x: 0.5 * x**1.5 - 2,
-            lambda x: 1 * np.exp(0.5 * (x - 2)) + 1
-        ]
-    )
+def piecewise3(num_points: int = 25, random_seed: int = None) -> np.ndarray:
+
+    def f(x):
+        return np.piecewise(
+            x, [x < 1, (x >= 1) & (x < 2), x >= 2],
+            [lambda x: 1 * x**2.5,
+             lambda x: 0.5 * x**1.5 - 2,
+             lambda x: 1 * np.exp(0.5 * (x - 2)) + 1]
+             )
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(0, 3, num_points)
+    else:
+        x = np.linspace(0, 3, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
 
 
-def nonsmooth1(x: np.ndarray) -> np.ndarray:
-    y_smooth = np.sin(0.7 * x) * (np.abs(x) >= 2)
-    y_non_smooth = np.sin(10 * x) * np.exp(-np.abs(2 * x)) * (np.abs(x) < 2)
-    return y_smooth + y_non_smooth
+def nonsmooth1(num_points: int = 25, random_seed: int = None) -> np.ndarray:
+
+    def f(x):
+        y_smooth = np.sin(0.7 * x) * (np.abs(x) >= 2)
+        y_non_smooth = np.sin(10 * x) * np.exp(-np.abs(2 * x)) * (np.abs(x) < 2)
+        return y_smooth + y_non_smooth
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(-7, 7, num_points)
+    else:
+        x = np.linspace(-7, 7, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
 
 
-def nonsmooth2(x):
-    transition = 1 / (1 + np.exp(-10 * (x - np.pi)))    
-    smooth_part = np.sin(2 * x)    
-    non_smooth_part = 0.1 * np.sin(10 * x) + 0.1 * np.cos(20 * x) + 0.3 * np.sin(x)**2    
-    return (1 - transition) * smooth_part + transition * non_smooth_part
+def nonsmooth2(num_points: int = 25, random_seed: int = None):
+
+    def f(x):
+        transition = 1 / (1 + np.exp(-10 * (x - np.pi))) 
+        smooth_part = np.sin(2 * x)
+        non_smooth_part = 0.1 * np.sin(10 * x) + 0.1 * np.cos(20 * x) + 0.3 * np.sin(x)**2
+        return (1 - transition) * smooth_part + transition * non_smooth_part
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(0, 5.5, num_points)
+    else:
+        x = np.linspace(0, 5.5, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
 
 
+def nonsmooth3(num_points: int = 25, random_seed: int = None):
+
+    def f(x):
+        f1 = np.sin(x)
+        f2 = 0.1 * (x - 5)**3
+        f3 = np.exp(0.5 * (x - 7))
+
+        t1 = sigmoid(x, 3.5, 20)  # Transition between f1 and f2
+        t2 = sigmoid(x, 4.5, 12)  # Transition between f2 and f3
+
+        return (1 - t1) * f1 + t1 * (1 - t2) * f2 + t2 * f3
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+        x = np.random.uniform(0, 8, num_points)
+    else:
+        x = np.linspace(0, 8, num_points)
+    x_test = np.linspace(x.min(), x.max(), 200)
+
+    return x, x_test, f(x), f(x_test)
+
+
+def sigmoid(x, x0, k):
+    return 1 / (1 + np.exp(-k * (x - x0)))
