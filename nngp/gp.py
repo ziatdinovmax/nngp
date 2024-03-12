ef9fb8d02f0bca45f1db02ba58f32fbc8c4c955e
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-03-12 19:06:31 +0000

    Use cholesly decomp

--- nngp/gp.py
+++ nngp/gp.py
@@ -1,5 +1,6 @@
 from typing import Dict, Tuple, Callable, Optional, Union
 from jax import vmap
+from jax.scipy import linalg
 import jax.random as jra
 import jax.numpy as jnp
 import numpyro
@@ -124,9 +125,9 @@ class GP:
         k_pp = self.kernel(X_new, X_new, params, noise_p, self.jitter)
         k_pX = self.kernel(X_new, X_train, params)
         # compute predictive mean covariance
-        K_xx_inv = jnp.linalg.inv(k_XX)
-        mean = jnp.matmul(k_pX, jnp.matmul(K_xx_inv, y_train))
-        cov = k_pp - jnp.matmul(k_pX, jnp.matmul(K_xx_inv, jnp.transpose(k_pX)))
+        K_xx_cho = linalg.cho_factor(k_XX)
+        mean = jnp.matmul(k_pX, linalg.cho_solve(K_xx_cho, self.y_train))
+        cov = k_pp - jnp.matmul(k_pX, linalg.cho_solve(K_xx_cho, k_pX.T))
         return mean, cov
 
     def predict(self,
