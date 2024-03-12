48c0a7aa993b0cb7bfe0d76e8925074d65835e40
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-03-12 19:00:13 +0000

    Remove dm-haiku

--- nngp/nn.py
+++ nngp/nn.py
@@ -1,7 +1,6 @@
 from typing import Callable, Dict, List
 import jax
 import jax.numpy as jnp
-import haiku as hk
 
 
 def get_mlp(hidden_dim: List[int], activation: str = 'tanh'
@@ -20,22 +19,3 @@ def get_mlp(hidden_dim: List[int], activation: str = 'tanh'
         z = jnp.matmul(h, params[f"w{len(hidden_dim)}"]) + params[f"b{len(hidden_dim)}"]
         return z
     return mlp
-
-
-class haiku_MLP(hk.Module):
-    def __init__(self, hidden_dim, latent_dim, activation='tanh', name=None):
-        super().__init__(name=name)
-        if activation not in ['relu', 'tanh']:
-            raise NotImplementedError("Use either 'relu' or 'tanh' for activation")
-        self.hidden_dim = hidden_dim
-        self.latent_dim = latent_dim
-        self.activation_fn = jnp.tanh if activation == 'tanh' else jax.nn.relu
-
-    def __call__(self, x):
-        # Process through each hidden layer
-        for size in self.hidden_dim:
-            x = hk.Linear(output_size=size)(x)
-            x = self.activation(x)
-        # The latent (output) layer
-        x = hk.Linear(output_size=self.latent_dim)(x)
-        return x
