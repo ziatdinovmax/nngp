1c1c30c392c23aba66a0583c14317bef186168fa
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-03-13 06:30:26 +0000

    Update synthetic functions

--- nngp/utils.py
+++ nngp/utils.py
@@ -55,7 +55,7 @@ def piecewise3(num_points: int = 25, random_seed: int = None) -> np.ndarray:
     return x, x_test, f(x), f(x_test)
 
 
-def nonsmooth1(num_points: int = 25, random_seed: int = None) -> np.ndarray:
+def nonstationary1(num_points: int = 25, random_seed: int = None) -> np.ndarray:
 
     def f(x):
         y_smooth = np.sin(0.7 * x) * (np.abs(x) >= 2)
@@ -72,7 +72,7 @@ def nonsmooth1(num_points: int = 25, random_seed: int = None) -> np.ndarray:
     return x, x_test, f(x), f(x_test)
 
 
-def nonsmooth2(num_points: int = 25, random_seed: int = None):
+def nonstationary2(num_points: int = 25, random_seed: int = None):
 
     def f(x):
         transition = 1 / (1 + np.exp(-10 * (x - np.pi))) 
@@ -90,16 +90,28 @@ def nonsmooth2(num_points: int = 25, random_seed: int = None):
     return x, x_test, f(x), f(x_test)
 
 
-def nonsmooth3(num_points: int = 25, random_seed: int = None):
+def nonstationary3(num_points: int = 25, random_seed: int = None):
 
     def f(x):
-        return np.sin(x) + x**2 / 10 - 2.5*np.exp(-50 * (x - 2)**2)
+        spike_params = [
+            (1, 2, 0.1),   # (amplitude, position, width)
+            (-0.5, 4, 0.15),
+            (1.5, 6, 0.1),
+            (-1, 8, 0.1),
+            (0.75, 9, 0.1)
+        ]
+        # Generate the smooth base curve
+        y = np.sin(x)
+        # Add each spike to the base curve
+        for a, b, c in spike_params:
+            y += a * np.exp(-((x - b)**2) / (2 * c**2))
+        return y
 
     if random_seed is not None:
         np.random.seed(random_seed)
-        x = np.random.uniform(-3, 5, num_points)
+        x = np.random.uniform(0, 10, num_points)
     else:
-        x = np.linspace(-3, 5, num_points)
+        x = np.linspace(0, 10, num_points)
     x_test = np.linspace(x.min(), x.max(), 200)
 
     return x, x_test, f(x), f(x_test)
